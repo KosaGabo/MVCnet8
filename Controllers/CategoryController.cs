63a59841318e59ed9ec991c57987030ddc67287a
Kósa Gábor <kosa.gabor@hydrasolutions.hu>
2024-04-12 00:51:37 +0200

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCnet8;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+}
