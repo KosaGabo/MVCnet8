7619cff86938ac712d7bbcce5a44f1cb9cb1c8e3
Kósa Gábor <kosa.gabor@hydrasolutions.hu>
2024-04-12 10:01:53 +0200

    UI update: theme+icon+create category site

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -10,9 +10,15 @@ public class CategoryController : Controller
     {
         _db = db;
     }
+    
     public IActionResult Index()
     {
         List<Category> objCategoryList = _db.Categories.ToList();
         return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
     }
 }
